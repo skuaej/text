ef7659015cd2e673df252e841c1449b13f8a76a2
skuaej <sumankumar821310@gmail.com>
2025-09-21 15:21:35 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -1,3 +1,13 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+port = int(os.getenv("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
 from TEAMZYRO import *
 import importlib
 import logging
